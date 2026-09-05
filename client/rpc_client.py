7d26137c0ccd2c44e0677f9f2f8df7b745b3a96d
Ariel Narciso <arielnarciso@gmail.com>
2026-09-05 09:53:00 -0300

    :recycle: nomes

--- client/rpc_client.py
+++ client/rpc_client.py
@@ -4,28 +4,28 @@ from xmlrpc.client import ServerProxy, Binary
 def start_client(host: str = 'localhost', port: int = 8000):
   print('Conectando ao Servidor Quoridor ...')
   server = ServerProxy(f'http://{host}:{port}')
-  clientId = cast(int, server.startConnection())
-  if clientId == -1:
+  client_id = cast(int, server.start_connection())
+  if client_id == -1:
     print('Servidor cheio! Limite de jogadores atingido.')
     return
-  print(f'Conectado com sucesso! Você é o jogador {clientId}.')
+  print(f'Conectado com sucesso! Você é o jogador {client_id}.')
   try:
     while True:
       myTime, data = cast(
-        tuple[bool, Binary], server.getGameState(clientId)
+        tuple[bool, Binary], server.get_game_state(client_id)
       )
       news = data.data.decode()
       if news:
         print(news)
       if myTime:
-        make_play(server, clientId)
+        make_play(server, client_id)
   except KeyboardInterrupt:
     print('\n Conexão com o servidor encerrada.')
 
-def make_play(server: ServerProxy, clientId: int):
+def make_play(server: ServerProxy, client_id: int):
   while True:
     res = input('Sua vez de jogar\nDiga a posição da barreira e orientação: ')
     pos, orientation = res.split(' ')
-    success = server.putWall(clientId, pos, int(orientation))
+    success = server.put_wall(client_id, pos, int(orientation))
     if success:
       break
--- server/game_manager.py
+++ server/game_manager.py
@@ -6,50 +6,53 @@ TOTAL_PLAYERS = 4
 class GameManager(Game):
 
 	def __init__(self):
-		self.nConnectedClients = 0
-		self.currentClientId = 1
+		self.n_connected_clients = 0
+		self.current_client_id = 1
 		self.news = ['Procurando jogadores ...\n' for _ in range(TOTAL_PLAYERS)]
 		self.news[TOTAL_PLAYERS - 1] = ''
-		self.__nWalls = [20 // TOTAL_PLAYERS for _ in range(TOTAL_PLAYERS)]
+		self.__n_walls = [20 // TOTAL_PLAYERS for _ in range(TOTAL_PLAYERS)]
 		super().__init__(['5A', '1E', '5I', '9E'], ['I', '9', 'A', '1'])
 
-	def startConnection(self):
-		if self.nConnectedClients < TOTAL_PLAYERS:
-			self.nConnectedClients += 1
-			if self.nConnectedClients == TOTAL_PLAYERS:
+	def start_connection(self):
+		if self.n_connected_clients < TOTAL_PLAYERS:
+			self.n_connected_clients += 1
+			if self.n_connected_clients == TOTAL_PLAYERS:
 				for i in range(TOTAL_PLAYERS):
 					self.news[i] += f'Jogadores conectados\n{super().__str__()}'
 					if i > 0:
 						self.news[i] += 'Aguardando lance do Player 1\n'
-			return self.nConnectedClients
+			return self.n_connected_clients
 		return -1
 
-	def getGameState(self, clientId: int) -> tuple[bool,Binary]:
-		news = self.news[clientId - 1]
-		self.news[clientId - 1] = ''
-		if self.nConnectedClients < TOTAL_PLAYERS:
+	def get_game_state(self, client_id: int) -> tuple[bool,Binary]:
+		news = self.news[client_id - 1]
+		self.news[client_id - 1] = ''
+		if self.n_connected_clients < TOTAL_PLAYERS:
 			return (False, Binary(news.encode()))
-		return (self.currentClientId == clientId, Binary(news.encode()))
+		return (self.current_client_id == client_id, Binary(news.encode()))
 
-	def getNWalls(self, clientId: int):
-		return self.__nWalls[clientId - 1]
+	def get_n_walls(self, client_id: int):
+		return self.__n_walls[client_id - 1]
 
-	def putWall(self, clientId: int, pos: str, orientation: int):
-		if self.currentClientId != clientId or self.__nWalls[clientId -1] == 0:
+	def put_wall(self, client_id: int, pos: str, orientation: int):
+		if self.current_client_id != client_id or self.__n_walls[client_id - 1] == 0:
 			return False
-		if not self.setBorder(pos, WallType.VERTICAL if orientation == 1 else WallType.HORIZONTAL):
+		if not self.set_wall(pos, WallType.VERTICAL if orientation == 1 else WallType.HORIZONTAL):
 			return False
-		self.__nWalls[clientId - 1] -= 1
-		nextPlayer = clientId % TOTAL_PLAYERS + 1
+		self.__n_walls[client_id - 1] -= 1
+		next_player = client_id % TOTAL_PLAYERS + 1
 		for i in range(TOTAL_PLAYERS):
 			self.news[i] = (
-				f'Player {clientId} colocou uma barreira em {pos}\n'
+				f'Player {client_id} colocou uma barreira em {pos}\n'
 				f'{super().__str__()}'
 			)
-			if i != nextPlayer - 1:
-				self.news[i] += f'Aguardando lance do Player {nextPlayer}\n'
-		self.currentClientId = nextPlayer
+			if i != next_player - 1:
+				self.news[i] += f'Aguardando lance do Player {next_player}\n'
+		self.current_client_id = next_player
 		return True
+
+	def move_pawn(self, client_id: int):
+		pass
 
 	# def movePawn(self, clientId: int);
 	# 	if self.__isWinner():
