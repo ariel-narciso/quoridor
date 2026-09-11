90e2bc1fd36f9bf474834684c0dcb2665aa685ba
Ariel Narciso <arielnarciso@gmail.com>
2026-09-11 13:53:33 -0300

    :sparkles: host e porta parametrizavel

--- run_client.py
+++ run_client.py
@@ -1,4 +1,13 @@
+from sys import argv
 from client.rpc_client import start_client
 
+def __main():
+    try:
+      host, port = argv[1:3]
+    except ValueError:
+      print('Informe o host e a porta')
+      return
+    start_client(host, int(port))
+
 if __name__ == '__main__':
-  start_client()
+   __main()
--- run_server.py
+++ run_server.py
@@ -1,7 +1,16 @@
+from sys import argv
 from server.rpc_server import start_server
 
-if __name__ == '__main__':
+def __main():
   try:
-    start_server()
+    host, port = argv[1:3]
+  except ValueError:
+    print('Informe o host e a porta')
+    return
+  try:
+    start_server(host, int(port))
   except KeyboardInterrupt:
     print('\n Servidor encerrado com sucesso')
+
+if __name__ == '__main__':
+  __main()
