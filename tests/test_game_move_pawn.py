c92c371b599d570ca5956346bde87de26ee51c4b
Ariel Narciso <arielnarciso@gmail.com>
2026-09-15 01:07:44 -0300

    :sparkles: testes de pulo duplo inválidos

--- tests/test_game_move_pawn.py
+++ tests/test_game_move_pawn.py
@@ -1,5 +1,5 @@
 import pytest
-from core.game import Game
+from core.game import Game, WallType
 
 class TestGameMovePawn:
   @pytest.fixture
@@ -10,6 +10,14 @@ class TestGameMovePawn:
   def pawns_together_scenario(self):
     return Game(['5E', '4E', '5F', '6E'], ['I', '9', 'A', '1'])
 
+  @pytest.fixture
+  def pawns_together_scenario_with_walls(self, pawns_together_scenario: Game):
+    pawns_together_scenario.set_wall('4D', WallType.HORIZONTAL)
+    pawns_together_scenario.set_wall('4E', WallType.VERTICAL)
+    pawns_together_scenario.set_wall('5G', WallType.VERTICAL)
+    pawns_together_scenario.set_wall('7E', WallType.HORIZONTAL)
+    return pawns_together_scenario
+
   @pytest.mark.parametrize('player_id, pos', [
     (1, '4A'), (1, '6A'), (1, '5B'),
     (2, '2E'), (2, '1D'), (2, '1F'),
@@ -18,21 +26,26 @@ class TestGameMovePawn:
   ])
   def test_valid_move_pawn(self, validator: Game, player_id: int, pos: str):
     assert validator.move_pawn(player_id, pos)
-    assert validator.player_positions[player_id - 1] == validator.convert_position(pos)
+    x, y = validator.convert_position(pos)
+    assert validator.player_positions[player_id - 1] == (x, y)
 
   @pytest.mark.parametrize('player_id, pos', [
     (1, '3A'), (2, '1G'), (3, '7I'), (4, '9C'),
   ])
   def test_invalid_move_pawn(self, validator: Game, player_id: int, pos: str):
+    x, y = validator.player_positions[player_id - 1]
     with pytest.raises(ValueError):
       validator.move_pawn(player_id, pos)
+    assert validator.player_positions[player_id - 1] == (x, y)
 
   @pytest.mark.parametrize('player_id, pos', [
     (1, '4A'), (2, '1D'), (3, '6I'), (4, '9F'), (2, '1E')
   ])
   def test_invalid_move_pawn_wall(self, walls_scenario: Game, player_id: int, pos: str):
+    x, y = walls_scenario.player_positions[player_id - 1]
     with pytest.raises(ValueError):
       walls_scenario.move_pawn(player_id, pos)
+    assert walls_scenario.player_positions[player_id - 1] == (x, y)
 
   @pytest.mark.parametrize('player_id, pos', [
     (1, '4E'), (1, '6E'), (1, '5F'),
@@ -40,13 +53,24 @@ class TestGameMovePawn:
     (2, '6E'), (4, '4E'),
   ])
   def test_invalid_move_pawn_filled_position(self, pawns_together_scenario: Game, player_id: int, pos: str):
+    x, y = pawns_together_scenario.player_positions[player_id - 1]
     with pytest.raises(ValueError):
       pawns_together_scenario.move_pawn(player_id, pos)
+    assert pawns_together_scenario.player_positions[player_id - 1] == (x, y)
 
   @pytest.mark.parametrize('player_id, pos', [
-    (1, '3E'), (1, '5G'), (3, '5D')
+    (1, '3E'), (1, '5G'), (1, '7E'), (3, '5D')
   ])
   def test_valid_move_pawn_double_jump(self, pawns_together_scenario: Game, player_id: int, pos: str):
     assert pawns_together_scenario.move_pawn(player_id, pos)
     x, y = pawns_together_scenario.convert_position(pos)
     assert pawns_together_scenario.player_positions[player_id - 1] == (x, y)
+
+  @pytest.mark.parametrize('player_id, pos', [
+    (1, '3E'), (1, '5G'), (1, '7E'), (3, '5D')
+  ])
+  def test_invalid_move_pawn_double_jump(self, pawns_together_scenario_with_walls: Game, player_id: int, pos: str):
+    x, y = pawns_together_scenario_with_walls.player_positions[player_id - 1]
+    with pytest.raises(ValueError):
+      assert pawns_together_scenario_with_walls.move_pawn(player_id, pos)
+    assert pawns_together_scenario_with_walls.player_positions[player_id - 1] == (x, y)
