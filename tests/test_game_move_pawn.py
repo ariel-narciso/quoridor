7110c155be3b5cef512440e27dc5b9987f7e1bed
Ariel Narciso <arielnarciso@gmail.com>
2026-09-16 17:20:45 -0300

    :sparkles: testa movimento diagonal do peão

--- tests/test_game_move_pawn.py
+++ tests/test_game_move_pawn.py
@@ -11,11 +11,11 @@ class TestGameMovePawn:
     return Game(['5E', '4E', '5F', '6E'], ['I', '9', 'A', '1'])
 
   @pytest.fixture
-  def pawns_together_scenario_with_walls(self, pawns_together_scenario: Game):
-    pawns_together_scenario.set_wall('4D', WallType.HORIZONTAL)
-    pawns_together_scenario.set_wall('4E', WallType.VERTICAL)
-    pawns_together_scenario.set_wall('5G', WallType.VERTICAL)
-    pawns_together_scenario.set_wall('7E', WallType.HORIZONTAL)
+  def pawns_together_with_walls_scenario(self, pawns_together_scenario: Game):
+    assert pawns_together_scenario.set_wall('4D', WallType.HORIZONTAL)
+    assert pawns_together_scenario.set_wall('4E', WallType.VERTICAL)
+    assert pawns_together_scenario.set_wall('5G', WallType.VERTICAL)
+    assert pawns_together_scenario.set_wall('7E', WallType.HORIZONTAL)
     return pawns_together_scenario
 
   @pytest.mark.parametrize('player_id, pos', [
@@ -69,8 +69,33 @@ class TestGameMovePawn:
   @pytest.mark.parametrize('player_id, pos', [
     (1, '3E'), (1, '5G'), (1, '7E'), (3, '5D')
   ])
-  def test_invalid_move_pawn_double_jump(self, pawns_together_scenario_with_walls: Game, player_id: int, pos: str):
-    x, y = pawns_together_scenario_with_walls.player_positions[player_id - 1]
+  def test_invalid_move_pawn_double_jump(self, pawns_together_with_walls_scenario: Game, player_id: int, pos: str):
+    x, y = pawns_together_with_walls_scenario.player_positions[player_id - 1]
     with pytest.raises(ValueError):
-      assert pawns_together_scenario_with_walls.move_pawn(player_id, pos)
-    assert pawns_together_scenario_with_walls.player_positions[player_id - 1] == (x, y)
+      pawns_together_with_walls_scenario.move_pawn(player_id, pos)
+    assert pawns_together_with_walls_scenario.player_positions[player_id - 1] == (x, y)
+
+  @pytest.mark.parametrize('player_id, pos', [
+    (1, '4F'), (1, '6D'), (1, '6F'),
+  ])
+  def test_valid_move_pawn_diagonal(self, pawns_together_with_walls_scenario: Game, player_id: int, pos: str):
+    assert pawns_together_with_walls_scenario.move_pawn(player_id, pos)
+    x, y = pawns_together_with_walls_scenario.convert_position(pos)
+    assert pawns_together_with_walls_scenario.player_positions[player_id - 1] == (x, y)
+
+  @pytest.mark.parametrize('player_id, pos', [
+    (1, '4D'), (2, '5D'), (4, '5D'),
+  ])
+  def test_invalid_move_pawn_diagonal(self, pawns_together_with_walls_scenario: Game, player_id: int, pos: str):
+    x, y = pawns_together_with_walls_scenario.player_positions[player_id - 1]
+    with pytest.raises(ValueError): 
+      pawns_together_with_walls_scenario.move_pawn(player_id, pos)
+    assert pawns_together_with_walls_scenario.player_positions[player_id - 1] == (x, y)
+
+  def test_custom_invalid_move_pawn_diagonal(self, pawns_together_with_walls_scenario: Game):
+    assert pawns_together_with_walls_scenario.set_wall('6D', WallType.HORIZONTAL)
+    x, y = pawns_together_with_walls_scenario.player_positions[0]
+    with pytest.raises(ValueError):
+      pawns_together_with_walls_scenario.move_pawn(1, '6D')
+      pawns_together_with_walls_scenario.move_pawn(1, '6F')
+    assert pawns_together_with_walls_scenario.player_positions[0] == (x, y)
